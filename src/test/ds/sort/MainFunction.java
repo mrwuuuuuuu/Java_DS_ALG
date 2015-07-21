aa57b2960659afc694109e8a2a05ebba90c3264f
Vincent Wu <wwyzxb@163.com>
2015-07-21 22:16:26 +0800

    归并排序

--- src/test/ds/sort/MainFunction.java
+++ src/test/ds/sort/MainFunction.java
@@ -2,13 +2,14 @@ package test.ds.sort;
 
 public class MainFunction {
 	public static void main(String[] args) {
-		int[] oldArray={49,38,65,97,76,13,27,49};
+		int[] oldArray={49,38,65,97,76,13,27,49,88,98,108};
 //		int[] newArray=InsertionSort.insertSort(oldArray);
 //		int[] newArray=SelectionSort.selectSort02(oldArray);
 //		int[] newArray=BubbleSort.bubbleSort01(oldArray);
 //		int[] newArray=ShellSort.shellSort(oldArray);
 //		int[] newArray=QuickSort.quickSort(oldArray, 0, oldArray.length-1);
-		int[] newArray=HeapSort.heapSort(oldArray);
+//		int[] newArray=HeapSort.heapSort(oldArray);
+		int[] newArray=MergeSort.mergeSort(oldArray,0,oldArray.length-1);
 		System.out.print("排序的结果为:");
 		for(int i=0;i<newArray.length;i++){
 			System.out.print(oldArray[i]+"\t");
--- src/test/ds/sort/MergeSort.java
+++ src/test/ds/sort/MergeSort.java
@@ -1,5 +1,42 @@
 package test.ds.sort;
 
 public class MergeSort {
+	private static void MergeArray(int array[],int p,int q,int r){
+		int i,j,k,n1,n2;
+		n1=q-p+1;
+		n2=r-q;
+		int[] L=new int[n1];
+		int[] R=new int[n2];
+		
+		for(i=0,k=p;i<n1;i++,k++) L[i]=array[k];
+		for(i=0,k=q+1;i<n2;i++,k++) R[i]=array[k];
+		for(k=p,i=0,j=0;i<n1&&j<n2;k++){
+			if(L[i]>R[j]){
+				array[k]=L[i];
+				i++;
+			}else {
+				array[k]=R[j];
+				j++;
+			}
+		}
+		if(i<n1){
+			for(j=i;j<n1;j++,k++)
+				array[k]=L[j];
+		}
+		if(j<n2){
+			for(i=j;i<n2;i++,k++)
+				array[k]=R[i];
+		}
+	}
+	
+	public static int[] mergeSort(int[] array,int p,int r){
+		if(p<r){
+			int q=(p+r)/2;
+			mergeSort(array, p, q);
+			mergeSort(array, q+1, r);
+			MergeArray(array, p, q, r);
+		}
+		return array;
+	}
 
 }
