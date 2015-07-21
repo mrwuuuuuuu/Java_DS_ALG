dfece3d928246f774868bfd8aeacf3cc0d6cbfe1
wwyzxb <wwyzxb@163.com>
2015-07-21 15:52:52 +0800

    main方法

--- src/test/ds/sort/MainFunction.java
+++ src/test/ds/sort/MainFunction.java
@@ -7,7 +7,8 @@ public class MainFunction {
 //		int[] newArray=SelectionSort.selectSort02(oldArray);
 //		int[] newArray=BubbleSort.bubbleSort01(oldArray);
 //		int[] newArray=ShellSort.shellSort(oldArray);
-		int[] newArray=QuickSort.quickSort(oldArray, 0, oldArray.length-1);
+//		int[] newArray=QuickSort.quickSort(oldArray, 0, oldArray.length-1);
+		int[] newArray=HeapSort.heapSort(oldArray);
 		System.out.print("排序的结果为:");
 		for(int i=0;i<newArray.length;i++){
 			System.out.print(oldArray[i]+"\t");
