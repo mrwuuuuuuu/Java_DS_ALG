e6abc735384717ce12f0803a7d79a329c4ab3a6a
lenovo <lenovo@lenovo-PC>
2015-07-24 22:22:05 +0800

    用数组/单链表实现栈

---
+++ src/test/ds/stack_queue/MainTest.java
@@ -0,0 +1,20 @@
+package test.ds.stack_queue;
+
+public class MainTest {
+	public static void main(String[] args) {
+		MyStack_Array stack_Array=new MyStack_Array();
+		stack_Array.push(1);
+		stack_Array.push(2);
+		stack_Array.push(3);
+		stack_Array.push(4);
+		stack_Array.push(5);
+		stack_Array.push(6);
+		stack_Array.push(7);
+		stack_Array.push(8);
+		stack_Array.push(9);
+		stack_Array.push(10);
+		stack_Array.push(10);
+		
+	}
+
+}
---
+++ src/test/ds/stack_queue/Node.java
@@ -0,0 +1,12 @@
+package test.ds.stack_queue;
+
+public class Node {
+	public Node next=null;
+	public Object data=null;
+	
+	public Node(){}
+	
+	public Node(Object data){
+		this.data=data;
+	}
+}
